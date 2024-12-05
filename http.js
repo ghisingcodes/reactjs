488fa37c55630a368976590af6339cbfa8181b30
Bijay Ghising <pytechnepal@gmail.com>
2024-12-05 09:49:23 +0545

    Updated

--- http.js
+++ http.js
@@ -7,3 +7,30 @@ export async function fetchAvaiablePlaces(){
 
     return resData.places;
 }
+
+export async function fetchUserPlaces(){
+    const response = await fetch("http://localhost:3000/user-places");
+    const resData = await response.json();
+    if(!response.ok){
+        throw new Error("Failed to fetch user places!");
+    }
+
+    return resData.places;
+}
+
+
+export async function updateUserPlaces(places){
+    const response = await fetch("http://localhost:3000/user-places", {
+        method: "PUT",
+        body: JSON.stringify({places}),
+        headers:{
+            "Content-Type": "application/json"
+        }
+    });
+
+    const resData = await response.json();
+    if(!response.ok){
+        throw new Error("Failed to update user data.");
+    }
+    return resData.message;
+}
